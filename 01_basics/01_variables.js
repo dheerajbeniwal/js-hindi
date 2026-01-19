1553b8d8bdff1b59e49da27ecc023d1d3b2b8d29
dheerajbeniwal <bairwadheeraj83@gmail.com>
2026-01-19 21:00:45 +0530

    fixed embedded git repo issue

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+console.log("hello world ");
+
+let accountId =1000
+const accountEmail="Dheeraj@45367gamil.com"
+var accountPassword ="55643"
+
+let accountState
+
+console.log(accountPassword);
+
+// accountEmail ="hell@gmail.com" it's not mutable 
+
+console.table([accountEmail,accountId,accountPassword,accountState])
---
+++ 01_basics/02_Data-types.js
@@ -0,0 +1,28 @@
+// alert("Hii There") <=  it's  working on any browser 
+
+// DATA TYPES
+/*
+
+1.number  =>2 to the power 53
+2.bigint
+3.string
+4.boolean  TRUE /False 
+5.null  => standalone vlue 
+6. undefined =>
+7.symbol //  the need unique dt //mostly use in React 
+
+
+*/
+
+// Object
+
+let name = "Dheeraj ";
+let password=2345678
+let yn = true
+let n =null
+let u ;
+
+
+console.table (typeof(el));
+
+console.table([typeof(name ),typeof(password),typeof(yn),typeof(n), typeof(u)])
